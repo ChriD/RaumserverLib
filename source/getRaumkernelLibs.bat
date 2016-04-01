c07664468542122120653673b736b0eb3e7406df
Christian Dürnberger <chris_d85@hotmail.com>
2016-04-01 22:04:04 +0200

    1.0.0

--- source/getRaumkernelLibs.bat
+++
@@ -1,7 +0,0 @@
-xcopy ..\..\RaumkernelLib\source\Debug\raumkernel.dll libs\windows_x86\Debug\raumkernel.dll /Y
-xcopy ..\..\RaumkernelLib\source\Debug\raumkernel.lib libs\windows_x86\Debug\raumkernel.lib /Y
-xcopy ..\..\RaumkernelLib\source\Release\raumkernel.dll libs\windows_x86\Release\raumkernel.dll /Y
-xcopy ..\..\RaumkernelLib\source\Release\raumkernel.lib libs\windows_x86\Release\raumkernel.lib /Y
-
-xcopy ..\..\RaumkernelLib\source\Debug\raumkernel.dll Debug\raumkernel.dll /Y
-xcopy ..\..\RaumkernelLib\source\Release\raumkernel.dll Release\raumkernel.dll /Y
