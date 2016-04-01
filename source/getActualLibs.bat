af616578832d895b3b4fe90c3e3c98134cd7b730
ChriD <chris_d85@hotmail.com>
2016-04-01 22:00:00 +0200

    1.0.0

---
+++ source/getActualLibs.bat
@@ -0,0 +1,7 @@
+xcopy ..\..\RaumkernelLib\source\Debug\raumkernel.dll libs\windows_x86\Debug\raumkernel.dll /Y
+xcopy ..\..\RaumkernelLib\source\Debug\raumkernel.lib libs\windows_x86\Debug\raumkernel.lib /Y
+xcopy ..\..\RaumkernelLib\source\Release\raumkernel.dll libs\windows_x86\Release\raumkernel.dll /Y
+xcopy ..\..\RaumkernelLib\source\Release\raumkernel.lib libs\windows_x86\Release\raumkernel.lib /Y
+
+xcopy ..\..\RaumkernelLib\source\Debug\raumkernel.dll Debug\raumkernel.dll /Y
+xcopy ..\..\RaumkernelLib\source\Release\raumkernel.dll Release\raumkernel.dll /Y
