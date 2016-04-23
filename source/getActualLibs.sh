f3d1288c10fff13b47f78c8bb55595bf184b3787
Christian Dürnberger <chris_d85@hotmail.com>
2016-04-23 14:00:25 +0200

    1.0.0

--- source/getActualLibs.sh
+++ source/getActualLibs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-cp ../../../RaumkernelLib/source/libs/linux_x86/raumkernel.so libs/linux_x86/raumkernel.so
+cp ../../RaumkernelLib/source/libs/linux_x86/raumkernel.so libs/linux_x86/raumkernel.so
 
-cp ../../../RaumkernelLib/source/libs/linux_ARMV6/raumkernel.so libs/linux_ARMV6/raumkernel.so
+cp ../../RaumkernelLib/source/libs/linux_ARMV6/raumkernel.so libs/linux_ARMV6/raumkernel.so
 
-cp ../../../RaumkernelLib/source/libs/linux_ARMV7/raumkernel.so libs/linux_ARMV7/raumkernel.so
+cp ../../RaumkernelLib/source/libs/linux_ARMV7/raumkernel.so libs/linux_ARMV7/raumkernel.so
 
